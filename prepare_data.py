2d4fc86221f36a73dfe66399dbdc2f0779a73979
Jannis Halbey <hateadd@web.de>
2020-07-25 23:29:16 +0200

    Changes from colab

--- prepare_data.py
+++ prepare_data.py
@@ -1,8 +1,15 @@
 import numpy as np
 
+num_lines = 1000
+
 with open('transcripts.csv', 'r', encoding='UTF-8') as input_file:
-    with open('processed_texts.csv', 'w', encoding='UTF-8') as output_file:    
+    with open('processed_texts_'+str(num_lines)+'.csv', 'w', encoding='UTF-8') as output_file:
         for i,line in enumerate(input_file):
             if i != 0:
-                output_file.write(line.split('","https://')[0][1:])
+                parts = line.split('","https://www.ted.com/talks/')
+                if len(parts) == 2:
+                    text = parts[0][1:].replace('""','"')
+                    output_file.write(text+'\n')
+                if i == num_lines:
+                    break
         
